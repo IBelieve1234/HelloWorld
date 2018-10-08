901d1c8509a96436ed22e2ca693e4b3e44ad05e6
IBelieve1234 <42485051+IBelieve1234@users.noreply.github.com>
2018-10-08 08:27:14 +0800

    Create 2Delete_redundant_information.h
    Create the second edition of Delete_redundant_information.h

---
+++ 2Delete_redundant_information.h
@@ -0,0 +1,21 @@
+//
+// Created by root on 18-10-7.
+//
+
+#ifndef AST_DELETE_REDUNDANT_INFORMATION_H
+#define AST_DELETE_REDUNDANT_INFORMATION_H
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<cstdlib>
+#include<vector>
+#include"Struct_Node.h"
+using namespace std;
+
+
+
+
+void Delete_redundant_information(string file_position);
+
+
+#endif //AST_DELETE_REDUNDANT_INFORMATION_H
